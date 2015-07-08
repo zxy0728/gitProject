e62beade3f2acc357224c19b9b18c4ed15f2b4a4
zxy0728 <zxy0728114@163.com>
2015-07-08 10:26:50 +0800

    Joseph circle
    Joseph circle, n people, k starts, on the count of m

---
+++ Joseph.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int yuesefu(int n,int k,int m);
+int main()
+{
+	int n,k,m,num;
+	cin>>n>>k>>m;
+	num=yuesefu(n,k,m);
+	cout<<"剩下的人是"<<num<<endl;
+	return 0;
+}
+int yuesefu(int n,int k,int m)
+{
+	int last=0;
+	for(int i=2;i<=n;i++)
+	{
+		last=(last+m+k-1)%i;
+	}
+	return last+1;
+}
